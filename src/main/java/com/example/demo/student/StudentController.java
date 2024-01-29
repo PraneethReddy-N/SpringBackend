b4357931738fc8a9dc30af3881673aa1e072a950
praneeth reddy <48400471+PraneethReddy-N@users.noreply.github.com>
2024-01-29 00:15:20 -0500

    Student Controller class to handle all requests

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,15 +1,25 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController 
 //here we route all our request from user
 public class StudentController {
     
+    @Autowired
+    private StudentService studentService;
 
-    @GetMapping
-    public String StudentInfo(){
-        return ":dsd";
-    } 
+    @PostMapping("/Register")
+        public Student saveStudent(@RequestBody Student student){
+            System.out.println("in controller" + student.getName() +" " +student.getStudentId());
+            return studentService.saveStudent(student);
+
+        }
+    
 }
