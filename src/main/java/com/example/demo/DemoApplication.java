0825527b864269b7817d2e27838bfe2de211283c
praneeth reddy <48400471+PraneethReddy-N@users.noreply.github.com>
2024-01-29 00:13:30 -0500

    commented getrequest.now handled in controller file seperately.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,10 +12,10 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@CrossOrigin(origins = "http://localhost:3000") // Use your frontend's origin here
-	@GetMapping
-	public String hello(@RequestParam(name = "name", defaultValue = "noOne")String name){
-		return "hello welcome " + name;
-	}
+	// @CrossOrigin(origins = "http://localhost:3000") // Use your frontend's origin here
+	// @GetMapping
+	// public String hello(@RequestParam(name = "name", defaultValue = "noOne")String name){
+	// 	return "hello welcome " + name;
+	// }
 
 }
